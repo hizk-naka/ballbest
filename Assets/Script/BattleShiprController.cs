e6ee95244158b6e26422a05653576b0861b0c6e5
hizk-naka <hiagikp@gmail.com>
2020-12-18 22:08:31 +0900

    プレイヤーの移動を追加

---
+++ Assets/Script/BattleShiprController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleShiprController : MonoBehaviour
+{
+    public float m_speed; // 移動の速さ
+
+    void Update()
+    {
+        // 矢印キーの入力情報を取得する
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        // 矢印キーが押されている方向にプレイヤーを移動する
+        var velocity = new Vector3(h, v) * m_speed;
+        transform.localPosition += velocity;
+    }
+}
